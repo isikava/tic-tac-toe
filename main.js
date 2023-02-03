329b714bbd1577f309c143bb908ea0d2044e319a
Alexander Ignatev <alexign473@gmail.com>
2023-02-03 14:37:36 +0300

    Add win conditions

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 const gameBoard = (function () {
-  const board = ['X', 'O', '', '', '', '', '', '', ''];
+  const board = ['X', 'X', '', '', '', '', '', '', ''];
+  const winCons = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
   const gameGrid = document.querySelector('#gameBoard');
 
   function render() {
@@ -10,19 +20,36 @@ const gameBoard = (function () {
     gameGrid.innerHTML = html;
   }
 
-  function update(index, marker) {
-    if (board[index] !== '') return;
+  /**
+   * @param {*} index
+   * @param {*} marker
+   * After click on the cell, getting cellIndex // 2
+   * Find all possible win conditions with that index // [0, 1, 2], [2, 5 ,6]
+   * Find some array in which each element in the board
+   * at an index equal to the array element
+   * is equal to the marker
+   * and return true or false
+   */
+  function isWinningTurn(index, marker) {
+    return winCons
+      .filter((cons) => cons.includes(index))
+      .some((con) => con.every((conEl) => board[conEl] === marker));
+  }
+
+  function makeTurn(index, marker) {
+    if (board[index] !== '') return false;
 
     board[index] = marker;
     render();
+
+    return isWinningTurn(index, marker);
   }
 
-  console.log('board init');
   render();
 
   return {
     gameGrid,
-    update,
+    makeTurn,
   };
 })();
 
@@ -33,20 +60,20 @@ const player = (name, marker) => ({
 
 // Game
 (() => {
-  const { gameGrid, update } = gameBoard;
-  const player1 = player('Boris', 'X1');
-  const player2 = player('Janna', 'O2');
-
-  let currentPlayer = player1;
-  currentPlayer = player2;
+  const { gameGrid, makeTurn } = gameBoard;
+  const player1 = player('Boris', 'X');
+  const player2 = player('Janna', 'O');
+  const currentPlayer = player2;
 
   const onClick = (e) => {
     // Get index of clicked cell
     const cellIndex = e.target.dataset.cell;
     if (!cellIndex) return;
 
-    // emit turn
-    update(cellIndex, currentPlayer.marker);
+    const isPlayerWon = makeTurn(+cellIndex, currentPlayer.marker);
+    if (isPlayerWon) {
+      console.log(`The winner is ${currentPlayer.name}`);
+    }
   };
 
   gameGrid.addEventListener('click', onClick);
